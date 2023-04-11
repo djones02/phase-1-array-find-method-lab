8d185e6f2ccb2247d5331d4150d5044e9f73792a
footballjones <footballjones308@gmail.com>
2023-04-11 13:32:09 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const resultW = record.find(record => record.result === "W")
+    if (resultW) {
+        return resultW.year
+    }
+}
